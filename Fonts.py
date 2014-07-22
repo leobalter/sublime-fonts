53b37f8df9fef90acc0342d54d1ca82137e4390b
Andre Fonseca <aoqfonseca@gmail.com>
2014-07-22 18:19:31 -0300

    Make working find fonts in mac os

--- Fonts.py
+++ Fonts.py
@@ -1,16 +1,11 @@
 import sublime
 import sublime_plugin
 import Cocoa
-# import platform, sys
 
-# ON_WINDOWS = platform.system() is 'Windows'
-# ST2 = sys.version_info < (3, 0)
-
-# if not ON_WINDOWS:
-    # not implemented yet...
 
 class Fonts(sublime_plugin.TextCommand):
-    def run(self):
-    	manager = Cocoa.NSFontManager.sharedFontManager()
-        self.show_panel(list(manager.availableFontFamilies())
 
+    def run(self, *args, **kwargs):
+    	manager = NSFontManager.sharedFontManager()
+		font_families = manager.availableFontFamilies()
+		sublime.message_dialog("%s" % font_families)
